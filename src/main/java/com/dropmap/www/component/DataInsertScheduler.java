ebe5d04891d06f284280bd32e816ec6040b069c9
shlee <shyuun@naver.com>
2025-08-19 09:38:43 +0900

    [완료]스케쥴러 실행

--- src/main/java/com/dropmap/www/component/DataInsertScheduler.java
+++ src/main/java/com/dropmap/www/component/DataInsertScheduler.java
@@ -4,6 +4,7 @@ import com.dropmap.www.service.DataProcessorService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.sql.SQLException;
@@ -13,7 +14,7 @@ import java.sql.SQLException;
 public class DataInsertScheduler {
     private final DataProcessorService dataProcessorService;
 
-    //@Scheduled(cron = "0 0 3 15 * *") // 매달 15일 03:00
+    @Scheduled(cron = "0 41 9 19 * *") // 매달 15일 03:00
     //@PostConstruct//for test init onetime
     private void run() throws JsonProcessingException, InterruptedException, SQLException {
         dataProcessorService.init();
