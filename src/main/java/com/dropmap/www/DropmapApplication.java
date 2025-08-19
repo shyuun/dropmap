6c55b9a06f99815c62869b9cf128e0c92d5a417e
shlee <shyuun@naver.com>
2025-08-19 10:07:51 +0900

    [완료]스케쥴러 실행3

--- src/main/java/com/dropmap/www/DropmapApplication.java
+++ src/main/java/com/dropmap/www/DropmapApplication.java
@@ -2,8 +2,10 @@ package com.dropmap.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DropmapApplication {
 
     public static void main(String[] args) {
