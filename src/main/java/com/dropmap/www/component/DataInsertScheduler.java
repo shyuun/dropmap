ac8b51f6a436d0fafd637694df461aa5219717f5
sh_home <shyuun@naver.com>
2025-10-30 23:25:18 +0900

    [완료] 데이터 INSERT실행

--- src/main/java/com/dropmap/www/component/DataInsertScheduler.java
+++ src/main/java/com/dropmap/www/component/DataInsertScheduler.java
@@ -4,6 +4,7 @@ import com.dropmap.www.service.DataProcessorService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,11 +12,12 @@ import java.sql.SQLException;
 
 @RequiredArgsConstructor
 @Component
+@ConditionalOnProperty(name = "app.scheduler.enabled", havingValue = "true")
 public class DataInsertScheduler {
     private final DataProcessorService dataProcessorService;
 
     //@Scheduled(cron = "0 0 3 15 * *") // 매달 15일 03:00
-    //@PostConstruct//for test init onetime
+    @PostConstruct//for test init onetime
     private void run() throws JsonProcessingException, InterruptedException, SQLException {
         dataProcessorService.init();
     }
